d946c44ff45ad48c9b812408266121e0b8efac17
Peterson Salme <petersonsalme@gmail.com>
2026-06-05 00:53:30 -0300

    test: add Spring Boot context load tests

---
+++ api-gateway/src/test/java/com/redhat/examples/MySpringBootApplicationTest.java
@@ -0,0 +1,14 @@
+package com.redhat.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootApplicationTest {
+
+    @Test
+    void contextLoads() {
+        // This test ensures that the Spring application context loads successfully.
+    }
+
+}
