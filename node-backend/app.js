123177278f89d463a266c6fc6167ddec6c4bf7b2
Peterson Salme <petersonsalme@gmail.com>
2026-06-05 00:53:30 -0300

    security: remove vulnerable ip package and refactor

--- node-backend/app.js
+++ node-backend/app.js
@@ -1,6 +1,20 @@
-var ip = require("ip");
+var os = require("os");
 var express = require("express");
 var app = express();
+
+function getIPAddress() {
+  var interfaces = os.networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+        return alias.address;
+      }
+    }
+  }
+  return '0.0.0.0';
+}
 
 app.get("/api/backend", (req, res, next) => {
   const { greeting } = req.query;
@@ -8,7 +22,7 @@ app.get("/api/backend", (req, res, next) => {
   const json = {
     greeting: greeting + " from Cluster Backend",
     time: new Date().getTime(),
-    ip: ip.address()
+    ip: getIPAddress()
   };
   
   res.json(json);
